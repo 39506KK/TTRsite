6367ed4e6cd1e5d62dea59aa0bfc3c1919bfb8c8
EC2 Default User <ec2-user@ip-172-31-10-35.ap-northeast-1.compute.internal>
2022-09-24 06:42:29 +0000

    コンフリクト解消

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-<<<<<<< HEAD
-module Portfolio
-=======
 module TTRsite
->>>>>>> refs/remotes/origin/develop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
