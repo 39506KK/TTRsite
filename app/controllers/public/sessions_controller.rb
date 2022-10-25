3837b2e3d45d080f423e78f43edd699e381e2725
39506KK <rurumaruku0214@gmail.com>
2022-10-25 14:51:37 +0000

    不要コード削除

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
 class Public::SessionsController < Devise::SessionsController
   
-  # before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
@@ -14,10 +11,6 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   protected
-  
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  # end
   
   # 退会しているかを判断するメソッド
   def customer_state
--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,4 @@ Rails.application.routes.draw do
   
   resources :maps, only:[:index]
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
